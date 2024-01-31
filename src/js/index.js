9992b43c13bd918f27dfe95b4807cc2d0f925386
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-01-30 22:39:09 -0300

    Input outline on acessibilidade

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-const formAtivos = document.querySelectorAll('.activo')
+const formAtivos = document.querySelectorAll('.activo');
 
 function enviarEChecar() {
 
     formAtivos.forEach((formAtivo) => {
         if (formAtivo.value) {
-            formAtivo.classList.add('avisoinputok')
-            formAtivo.nextElementSibling.classList.remove('avisoerror')
+            formAtivo.classList.add('avisoinputok');
+            formAtivo.nextElementSibling.classList.remove('avisoerror');
         } else {
-            formAtivo.classList.remove('avisoinputok')
-            formAtivo.classList.add('avisoinputerror')
-            formAtivo.nextElementSibling.classList.add('avisoerror')
-        }
-    })
-}
+            formAtivo.classList.remove('avisoinputok');
+            formAtivo.classList.add('avisoinputerror');
+            formAtivo.nextElementSibling.classList.add('avisoerror');
+        };
+    });
+};
